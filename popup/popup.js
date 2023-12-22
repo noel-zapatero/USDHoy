042e2cf7181d11af6a485b3e45c173d863dedeed
Noel Zapatero <NoelZapatero@users.noreply.github.com>
2023-12-22 12:43:58 -0300

    listado de usd

--- popup/popup.js
+++ popup/popup.js
@@ -1,25 +1,82 @@
-var dolares;
+const LS = chrome.storage.local;
 
-async function logUSD() {
+var dolares;
+var checkedUSD = [];
+
+async function getUSD() {
 	const response = await fetch("https://dolarapi.com/v1/dolares")
 	dolares = await response.json();
 	console.log(dolares);
 
-	getOficial();
-	getBlue();
+	createList();
+	listenCheckboxes();
+	updateView();
+	checkLocalStorage();
 }
 
-function getOficial(){
-	let ofi = dolares.find(d => d.casa == 'oficial');
-	document.getElementById('oficialCompra').innerHTML = ofi.compra;
-	document.getElementById('oficialVenta').innerHTML = ofi.venta;
+function createList(){
+	let menu = document.getElementById("menu");
+	for (i = 0; i < dolares.length; ++i) {
+		let label= document.createElement("label");
+		let description = document.createTextNode(dolares[i].nombre);
+		let checkbox = document.createElement("input");
+
+		checkbox.type = "checkbox";
+		checkbox.classList.add("checkMenu")
+
+		label.appendChild(checkbox);
+		label.appendChild(description);
+
+		let li = document.createElement("li");
+		li.appendChild(label);
+
+		menu.appendChild(li);
+	}
 }
 
-function getBlue(){
-	let blue = dolares.find(d => d.casa == 'blue');
-	document.getElementById('blueCompra').innerHTML = blue.compra;
-	document.getElementById('blueVenta').innerHTML = blue.venta;
+function listenCheckboxes(){
+	let checks = document.getElementsByClassName("checkMenu");
+	for(i=0; i < checks.length; i++){
+		checks[i].addEventListener('change', (event) => {
+			const elem = event.currentTarget;
+			const label = elem.parentNode.textContent;
+			if (elem.checked) {
+				checkedUSD.push(label);
+			}else{
+				checkedUSD.splice(checkedUSD.indexOf(label), 1);
+			}
+
+			updateView();
+		})
+	}
 }
 
+async function checkLocalStorage(){
+	await LS.get(["dolarHoyArg"], function(items){
+		if(items != undefined && items != ''){
+			checkedUSD = JSON.parse(items.dolarHoyArg);
+			updateView();
+		}
+	});
+}
 
-logUSD();
+async function updateView(){
+	if(checkedUSD.length > 0){
+		let view = document.getElementById("view");
+		view.innerHTML = '';
+		for(i=0; i < checkedUSD.length; i++){
+			let li = document.createElement('li')
+			li.innerHTML += `<div><details><summary><h3>Dolar ${checkedUSD[i]}</h3></summary><b>Compra:</b> $<span id="${checkedUSD[i].toLowerCase()}Compra"></span><span style="margin: 0px 5px"><b>|</b></span><b>Venta:</b> $<span id="${checkedUSD[i].toLowerCase()}Venta"></span></details></div>`
+
+			view.appendChild(li);
+			
+			let usd = dolares.find(d => d.nombre == checkedUSD[i]);
+			document.getElementById(usd.casa + 'Compra').innerHTML = usd.compra;
+			document.getElementById(usd.casa + 'Venta').innerHTML = usd.venta;
+		}
+
+		await LS.set({'dolarHoyArg': JSON.stringify(checkedUSD)})
+	}
+}
+
+getUSD();
